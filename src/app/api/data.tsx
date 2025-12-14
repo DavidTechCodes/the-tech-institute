6445b670497ddabdbbf92ddc836b4acd35dc0aeb
DextrousDavid <aimakhededavido@gmail.com>
2025-12-14 23:03:00 +0000

    Staging for prod

---
+++ src/app/api/data.tsx
@@ -0,0 +1,169 @@
+export const TruestedCompanies: { imgSrc: string }[] = [
+  {
+    imgSrc: "/images/companies/airbnb.svg"
+  },
+  {
+    imgSrc: "/images/companies/fedex.svg"
+  },
+  {
+    imgSrc: "/images/companies/google.svg"
+  },
+  {
+    imgSrc: "/images/companies/hubspot.svg"
+  },
+  {
+    imgSrc: "/images/companies/microsoft.svg"
+  },
+  {
+    imgSrc: "/images/companies/walmart.svg"
+  },
+  {
+    imgSrc: "/images/companies/airbnb.svg"
+  },
+  {
+    imgSrc: "/images/companies/fedex.svg"
+  }
+]
+
+export const courseData: {
+  heading: string;
+  imgSrc: string;
+  name: string;
+  students: number;
+  classes: number;
+  price: number;
+  rating: number;
+}[] = [
+    {
+      heading: 'Full stack modern javascript',
+      name: "Brad Traversy",
+      imgSrc: '/images/courses/courseone.png',
+      students: 84,
+      classes: 3,
+      price: 20,
+      rating: 4.4,
+    },
+    {
+      heading: 'Design system with React programme',
+      name: "David Aimakhede",
+      imgSrc: '/images/courses/coursetwo.png',
+      students: 37,
+      classes: 8,
+      price: 19.9,
+      rating: 4.5,
+    },
+    {
+      heading: 'Design banner with Figma',
+      name: "Sarah Taylor",
+      imgSrc: '/images/courses/coursethree.png',
+      students: 120,
+      classes: 12,
+      price: 20,
+      rating: 5,
+    },
+    {
+      heading: 'Introduction to ITIL Practitioner',
+      name: "Mike Russell",
+      imgSrc: '/images/courses/courseone.png',
+      students: 45,
+      classes: 1,
+      price: 95,
+      rating: 5,
+    },
+    {
+      heading: 'The Web Handbook for Developers',
+      name: "David Aimakhede",
+      imgSrc: '/images/courses/coursetwo.png',
+      students: 150,
+      classes: 75,
+      price: 10.99,
+      rating: 5,
+    },
+    {
+      heading: 'Cyber Security for Beginners',
+      name: "Colt stelle",
+      imgSrc: '/images/courses/coursethree.png',
+      students: 170,
+      classes: 22,
+      price: 45,
+      rating: 4.2,
+    },
+  ]
+
+export const MentorData: { profession: string; name: string; imgSrc: string }[] = [
+  {
+    profession: 'Senior UX Designer',
+    name: 'Victor Crest',
+    imgSrc: '/images/mentor/user3.png',
+  },
+  {
+    profession: 'Software Developer',
+    name: 'Jane Cooper',
+    imgSrc: '/images/mentor/user2.png',
+  },
+  {
+    profession: 'DevOps Engineer',
+    name: 'Shoo Thar Mien',
+    imgSrc: '/images/mentor/user1.png',
+  },
+  // {
+  //   profession: 'Senior UX Designer',
+  //   name: 'Shoo Thar Mien',
+  //   imgSrc: '/images/mentor/user3.png',
+  // },
+  // {
+  //   profession: 'Senior UX Designer',
+  //   name: 'Shoo Thar Mien',
+  //   imgSrc: '/images/mentor/user2.png',
+  // },
+  // {
+  //   profession: 'Senior UX Designer',
+  //   name: 'Shoo Thar Mien',
+  //   imgSrc: '/images/mentor/user1.png',
+  // },
+]
+
+export const TestimonialData: { profession: string; comment: string; imgSrc: string; name: string; rating: number; }[] = [
+  {
+    name: "Robert Fox",
+    profession: 'CEO, Parkview Int.Ltd',
+    comment: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+    imgSrc: '/images/testimonial/user.svg',
+    rating: 5
+  },
+  {
+    name: "Leslie Alexander",
+    profession: 'CEO, Parkview Int.Ltd',
+    comment: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+    imgSrc: '/images/mentor/user2.png',
+    rating: 5
+  },
+  {
+    name: "Cody Fisher",
+    profession: 'CEO, Parkview Int.Ltd',
+    comment: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+    imgSrc: '/images/mentor/user3.png',
+    rating: 5
+  },
+  {
+    name: "Robert Fox",
+    profession: 'CEO, Parkview Int.Ltd',
+    comment: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+    imgSrc: '/images/mentor/user1.png',
+    rating: 5
+  },
+  {
+    name: "Leslie Alexander",
+    profession: 'CEO, Parkview Int.Ltd',
+    comment: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+    imgSrc: '/images/mentor/user2.png',
+    rating: 5
+  },
+  {
+    name: "Cody Fisher",
+    profession: 'CEO, Parkview Int.Ltd',
+    comment: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour',
+    imgSrc: '/images/mentor/user3.png',
+    rating: 5
+  },
+]
---
+++ src/components/Layout/Footer/index.tsx
@@ -0,0 +1,119 @@
+import Link from "next/link";
+import Image from "next/image";
+import Logo from "../Header/Logo";
+import { Icon } from "@iconify/react/dist/iconify.js";
+import { headerData } from "../Header/Navigation/menuData";
+
+const footer = () => {
+  return (
+    <footer className="bg-deepSlate py-10">
+      <div className="container mx-auto lg:max-w-screen-xl md:max-w-screen-md px-4">
+        <div className="grid grid-cols-1 gap-y-10 gap-x-16 sm:grid-cols-2 lg:grid-cols-12 xl:gap-x-8">
+          <div className='col-span-4 md:col-span-12 lg:col-span-4'>
+            <Link href="/" className="flex items-center group select-none mb-4">
+            <span className="text-2xl font-bold tracking-tight text-slate-900">
+              The
+              <span className="text-blue-600 mx-0.5">
+                Tech
+              </span>
+              Institute
+            </span>
+          </Link>
+            <div className='flex items-center gap-4'>
+              <Link href="#" className='hover:text-primary text-black text-3xl'>
+                <Icon
+                  icon="tabler:brand-facebook"
+                />
+              </Link>
+              <Link href="#" className='hover:text-primary text-black text-3xl'>
+                <Icon
+                  icon="tabler:brand-twitter"
+                />
+              </Link>
+              <Link href="#" className='hover:text-primary text-black text-3xl'>
+                <Icon
+                  icon="tabler:brand-instagram"
+                />
+              </Link>
+            </div>
+          </div>
+          <div className="col-span-2">
+            <h3 className="mb-4 text-2xl font-medium text-black">Links</h3>
+            <ul>
+              {headerData.map((item, index) => (
+                <li key={index} className="mb-2 text-black/50 hover:text-primary w-fit">
+                  <Link href={item.href}>
+                    {item.label}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div className="col-span-2">
+            <h3 className="mb-4 text-2xl font-medium text-black">Other</h3>
+            <ul>
+              <li className="mb-2 text-black/50 hover:text-primary w-fit">
+                <span>
+                  About Us
+                </span>
+              </li>
+              <li className="mb-2 text-black/50 hover:text-primary w-fit">
+                <span>
+                  Our Team
+                </span>
+              </li>
+              <li className="mb-2 text-black/50 hover:text-primary w-fit">
+                <span>
+                  career
+                </span>
+              </li>
+              <li className="mb-2 text-black/50 hover:text-primary w-fit">
+                <span>
+                  Services
+                </span>
+              </li>
+              <li className="mb-2 text-black/50 hover:text-primary w-fit">
+                <span>
+                  Contact
+                </span>
+              </li>
+            </ul>
+          </div>
+          <div className='col-span-4 md:col-span-4 lg:col-span-4'>
+            <div className="flex items-center gap-2">
+              <Icon
+                icon="tabler:brand-google-maps"
+                className="text-primary text-3xl inline-block me-2"
+              />
+              <h5 className="text-lg text-black/60">504 Fillo Street Pennsylvan 1072</h5>
+            </div>
+            <div className="flex gap-2 mt-10">
+              <Icon
+                icon="tabler:phone"
+                className="text-primary text-3xl inline-block me-2"
+              />
+              <h5 className="text-lg text-black/60">+44 7354 290129</h5>
+            </div>
+            <div className="flex gap-2 mt-10">
+              <Icon
+                icon="tabler:folder"
+                className="text-primary text-3xl inline-block me-2"
+              />
+              <h5 className="text-lg text-black/60">aimakhededavido@gmail.com</h5>
+            </div>
+          </div>
+        </div>
+
+        <div className='mt-10 lg:flex items-center justify-between'>
+          <h4 className='text-black/50 text-sm text-center lg:text-start font-normal'>David Aimakhede @ { new Date().getFullYear() }. All Rights Reserved</h4>
+          <div className="flex gap-5 mt-5 lg:mt-0 justify-center lg:justify-start">
+            <Link href="/" className='text-black/50 text-sm font-normal hover:text-primary'>Privacy policy</Link>
+            <Link href="/" className='text-black/50 text-sm font-normal hover:text-primary'>Terms & conditions</Link>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default footer;
